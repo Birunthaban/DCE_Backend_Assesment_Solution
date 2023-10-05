ac5a3616260bc31182c434541b65c54acf3db266
Birunthaban Sarventhiran <birunth-se19006@stu.kln.ac.lk>
2023-10-05 20:35:07 +0530

    Change Launch Setting to show a welcome message and some a dummy controller for that

---
+++ DCE_Backend_Developer_Assesment/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DCE_Backend_Developer_Assesment.Controllers
+{
+    [Route("home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorld() {
+            return Ok("Hello World");
+        }
+    }
+}
