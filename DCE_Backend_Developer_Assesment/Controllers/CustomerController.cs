985be3e3be5bafdc0a3b7edcc4364787ebbf2422
Birunthaban Sarventhiran <birunth-se19006@stu.kln.ac.lk>
2023-10-05 14:30:36 +0530

    Make the error responses consistent

--- DCE_Backend_Developer_Assesment/Controllers/CustomerController.cs
+++ DCE_Backend_Developer_Assesment/Controllers/CustomerController.cs
@@ -3,7 +3,10 @@ using DCE_Backend_Developer_Assesment.Models;
 using DCE_Backend_Developer_Assesment.Repositories;
 using DCE_Backend_Developer_Assesment.Services;
 using DCE_Backend_Developer_Assessment.DTO.Requests;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace DCE_Backend_Developer_Assesment.Controllers
 {
@@ -14,7 +17,7 @@ namespace DCE_Backend_Developer_Assesment.Controllers
         private readonly ICustomerService _customerService;
         private readonly IOrderService _orderService;
 
-        public CustomerController(ICustomerService customerService ,IOrderService orderService)
+        public CustomerController(ICustomerService customerService, IOrderService orderService)
         {
             _customerService = customerService;
             _orderService = orderService;
@@ -25,38 +28,47 @@ namespace DCE_Backend_Developer_Assesment.Controllers
         {
             var customers = _customerService.GetAll();
 
-            // Check if the customers collection is empty or null
             if (customers == null || !customers.Any())
             {
-                return NotFound(); // Return a 404 Not Found response if no customers are found
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status404NotFound
+                };
+                return BadRequest(problemDetails);
             }
 
-            return Ok(customers); // Return a 200 OK response with the list of customers
+            return Ok(customers);
         }
+
         [HttpPost("register")]
         public IActionResult RegisterCustomer([FromBody] CustomerRegistrationRequest registrationRequest)
         {
-            // Validate the incoming registration request data
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState); // Return validation errors
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                return BadRequest(problemDetails);
             }
 
-            // Pass the registration request to the service for registration
             Customer createdCustomer = _customerService.RegisterCustomer(registrationRequest);
 
             if (createdCustomer != null)
             {
-                // Return an HTTP Created (201) response with the created customer object
                 return CreatedAtAction(nameof(GetCustomerById), new { id = createdCustomer.UserId }, createdCustomer);
             }
             else
             {
-                // Handle registration failure (e.g., duplicate email)
-                // Return an appropriate response
-                return BadRequest(new { Message = "Registration failed" });
+                ModelState.AddModelError("RegistrationError", "Registration failed");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                return BadRequest(problemDetails);
             }
         }
+
         [HttpDelete("{id}")]
         public IActionResult DeleteCustomer(Guid id)
         {
@@ -64,45 +76,64 @@ namespace DCE_Backend_Developer_Assesment.Controllers
 
             if (deleted)
             {
-                return NoContent(); // Return a 204 No Content response on successful delete
+                return NoContent();
             }
             else
             {
-                return NotFound(); // Return a 404 Not Found response if the customer is not found
+                ModelState.AddModelError("CustomerNotFound", "Customer not found");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status404NotFound
+                };
+                return BadRequest(problemDetails);
             }
         }
-
 
         [HttpGet("{id}", Name = "GetCustomerById")]
         public IActionResult GetCustomerById(Guid id)
         {
-            // Implement logic to retrieve a customer by ID using your service/repository
             Customer customer = _customerService.GetCustomerById(id);
 
             if (customer == null)
             {
-                return NotFound(); // Return a 404 Not Found response if the customer is not found
+                ModelState.AddModelError("CustomerNotFound", "Customer not found");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status404NotFound
+                };
+                return BadRequest(problemDetails);
             }
 
-            return Ok(customer); // Return a 200 OK response with the customer object
+            return Ok(customer);
         }
+
         [HttpPut("{id}")]
         public IActionResult UpdateCustomer(Guid id, [FromBody] CustomerUpdateRequest updateRequest)
         {
             if (id != updateRequest.UserId)
             {
-                return BadRequest("Customer ID mismatch."); // Return a 400 Bad Request if the provided ID doesn't match the updated customer's ID
+                ModelState.AddModelError("CustomerIdMismatch", "Customer ID mismatch");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                return BadRequest(problemDetails);
             }
 
             bool updated = _customerService.UpdateCustomer(id, updateRequest.Username, updateRequest.Email, updateRequest.FirstName, updateRequest.LastName);
 
             if (updated)
             {
-                return NoContent(); // Return a 204 No Content response on successful update
+                return NoContent();
             }
             else
             {
-                return BadRequest("Email is already in use or no fields to update."); // Return a 400 Bad Request if the email is already in use or no fields to update
+                ModelState.AddModelError("UpdateError", "Email is already in use or no fields to update");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status400BadRequest
+                };
+                return BadRequest(problemDetails);
             }
         }
 
@@ -113,17 +144,15 @@ namespace DCE_Backend_Developer_Assesment.Controllers
 
             if (activeOrders == null || !activeOrders.Any())
             {
-                return NotFound(); // Return a 404 Not Found response if no active orders are found
+                ModelState.AddModelError("NoActiveOrders", "No active orders found");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status404NotFound
+                };
+                return BadRequest(problemDetails);
             }
 
-            return Ok(activeOrders); // Return a 200 OK response with the list of active orders
+            return Ok(activeOrders);
         }
-
-
-
-
-
     }
-
 }
-
--- DCE_Backend_Developer_Assesment/Program.cs
+++ DCE_Backend_Developer_Assesment/Program.cs
@@ -1,7 +1,8 @@
-
 using DCE_Backend_Developer_Assesment.Repositories;
 using DCE_Backend_Developer_Assesment.Services;
-using Microsoft.Extensions.Configuration; // Add this using statement
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection; // Add this using statement
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,26 @@ builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Configuration.AddJsonFile("appsettings.json"); // Load settings from appsettings.json
 // You can add more configuration sources as needed (e.g., environment variables)
 
+// Configure ApiBehaviorOptions to use ModelState for validation errors
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(e => e.Value.Errors.Count > 0)
+            .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
+            );
+
+        return new Microsoft.AspNetCore.Mvc.BadRequestObjectResult(new
+        {
+            Message = "Validation failed.",
+            Errors = errors
+        });
+    };
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
@@ -28,3 +49,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
